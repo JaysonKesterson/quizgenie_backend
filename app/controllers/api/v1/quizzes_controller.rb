fe527a160ff1cd38cb51e1a844deda04538d6030
jayson_kesterson <your_email@example.com>
2020-06-02 17:52:45 -0400

    quiz controller with index and create actions, with appropriate routing

--- app/controllers/api/v1/quizzes_controller.rb
+++ app/controllers/api/v1/quizzes_controller.rb
@@ -1,2 +1,21 @@
 class Api::V1::QuizzesController < ApplicationController
+    def index
+        quizzes = Quiz.all
+        render json: quizzes
+    end
+
+    def create
+        quiz = Quiz.new(quiz_params)
+        if quiz.save
+            render json: quiz, status: :accepted
+        else
+            render json: {errors: quiz.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def quiz_params
+        params.require(:quiz).permit(:name, :category)
+    end
 end
