44a0a3c6567ba49dbd04179fd0c00e1b5e1abbca
jayson_kesterson <your_email@example.com>
2020-06-05 12:30:28 -0400

    attempting to solve issue with question creation

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,6 +1,21 @@
 class Api::V1::QuestionsController < ApplicationController
     def index
         questions = Question.all
-        render json: QuestionSerializer.new(quizzes)
+        render json: QuestionSerializer.new(questions)
+    end
+
+    def create
+        question = Question.new(question_params)
+        if question.save
+            render json: question, status: :accepted
+        else
+            render json: {errors: question.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def question_params
+        params.require(:question).permit(:content, :answer, :quiz_id)
     end
 end
