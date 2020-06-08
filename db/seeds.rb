fa2950ab65823890329d3af4b239131de60edfa8
jayson_kesterson <your_email@example.com>
2020-06-08 09:14:42 -0400

    modifying seed data for category

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-rainbow_quiz = Quiz.create(name: "Rainbow Quiz", category: "Miscellaneous")
+rainbow_quiz = Quiz.create(name: "Rainbow Quiz", category: "random-knowledge")
 Question.create(content: "What is the first color of the Rainbow?", answer: "red", quiz_id: 1)
 Question.create(content: "What is the second color of the Rainbow?", answer: "orange", quiz_id: 1)
 Question.create(content: "What is the third color of the Rainbow?", answer: "yellow", quiz_id: 1)
